aab1b1252711fd33b3780130ae1ff0ee2f31f317
Joe W. Byers <ecjbosu@aol.com>
2014-09-23 13:21:34 -0500

    Update perlBusinessDayInstall.sh
    added comments  and oldk perl instal

--- perlBusinessDayInstall.sh
+++ perlBusinessDayInstall.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+#cpan must be install, otherwise download the Date::Business package, cd to directory, and do next section.
 cpan get Date::Business
 cpan make Date::Business
 cpan install Date::Business
+
+#perl Makefile.PL
+#make
+#make install
 
